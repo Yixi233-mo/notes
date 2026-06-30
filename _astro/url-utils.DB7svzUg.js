9c13c61792711bfc90d3a7c94c7025c7df3c0708
Yixi233-mo <3616721518@qq.com>
2026-06-30 09:06:47 +0000

    Deploying to pages from @ Yixi233-mo/notes@c34a67d82a170d2f9f775294d1c2fc98a6334782 🚀

---
+++ _astro/url-utils.DB7svzUg.js
@@ -0,0 +1,1 @@
+import"./translation.CMH-QElI.js";import"./config.BYnwfVvC.js";function a(r,e){return r.replace(/^\/|\/$/g,"").toLowerCase()===e.replace(/^\/|\/$/g,"").toLowerCase()}function t(...r){return r.join("/").replace(/\/+/g,"/")}function u(r){return t("","/notes/",r)}export{u as n,a as t};
